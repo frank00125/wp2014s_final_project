ab87afb457113d9856602aa9c7873c86ebca3cd4
s914211 <cchccy1623@gmail.com>
2014-06-15 23:08:16 +0800

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,7 +1,7 @@
 Parse.initialize("3zNjT9EGuUYzq0Ucqj9mrYOZBQQri1u40LqDGhiJ","FhvDpueqCRBp1bvNDRL7Scbb00J9f7KoyQMmlnvC");
 
       
-$('#localLogin').submit(function(){
+$('#submit').click(function(){
         var user = new Parse.User();
             user.set("username", document.getElementById('email').value);
             user.set("password", document.getElementById('password').value);
