00792ba79c0208cee400a52067ae92c4cc9760e2
frank00125 <frank9732@gmail.com>
2014-06-15 20:40:29 +0800

    Version 2.5

---
+++ signup.js
@@ -0,0 +1,15 @@
+Parse.initialize("3zNjT9EGuUYzq0Ucqj9mrYOZBQQri1u40LqDGhiJ","FhvDpueqCRBp1bvNDRL7Scbb00J9f7KoyQMmlnvC");
+
+document.getElementById("localLogin").addEventListener("submit",function(){
+        var user = new Parse.User();
+            user.set("username", document.getElementById('email').value);
+            user.set("password", document.getElementById('password').value);
+            user.signUp(null, {
+                success: function(user){
+                  window.location.assign("LoginPage.html");
+                },
+                error: function(user, error){
+                  alert('fail');
+                }
+            });
+      })
