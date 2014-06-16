86a09eb6e4f4d566366198dee3d07e10d540a88f
s914211 <cchccy1623@gmail.com>
2014-06-16 14:10:14 +0800

    Update My_Card.js

--- My_Card.js
+++ My_Card.js
@@ -57,6 +57,16 @@ $(document).ready(function(){
 
 
 $('#logout').click(function(){
+	
+	window.fbAsyncInit = function() {
+  		FB.init({
+		 appId      : '243758945748336',
+		 cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+		 xfbml      : true,  // parse social plugins on this page
+		 version    : 'v2.0' // use version 2.0
+  		});
+	
 	FB.getLoginStatus(function (response){
 		if(response.status === 'connected'){
 			FB.logout(function(response){
@@ -71,4 +81,13 @@ $('#logout').click(function(){
 			alert("請重新登入！");
 		}
 	});
+	};
+	
+	(function(d, s, id) {
+		 var js, fjs = d.getElementsByTagName(s)[0];
+    		if (d.getElementById(id)) return;
+    		js = d.createElement(s); js.id = id;
+    		js.src = "//connect.facebook.net/en_US/sdk.js";
+		 fjs.parentNode.insertBefore(js, fjs);
+  		}(document, 'script', 'facebook-jssdk'));
 });
