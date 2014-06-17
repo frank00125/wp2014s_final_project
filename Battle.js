def61592b552e63eccbff875d6ef8870b1777168
s914211 <cchccy1623@gmail.com>
2014-06-17 19:50:35 +0800

    Update Battle.js

--- Battle.js
+++ Battle.js
@@ -27,14 +27,13 @@ $(document).ready(function(){
 		});*/
 		
 		var randomuser = [];
-	var BattleCard = Parse.Object.extend('BattleCard');
+		var BattleCard = Parse.Object.extend('BattleCard');
 		var query = new Parse.Query(BattleCard);
 		query.include('card','user','flow');
 		query.find({
 			success: function(data) {
 				for(var i=0;i <data.length;i++){
-					randomuser = data[i];
-					console.log(data[i]);
+					console.log(data);
 				}
 			}
 		});
