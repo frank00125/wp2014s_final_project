4f711998500779bc8ec66c9e378955e2561eb5b1
s914211 <cchccy1623@gmail.com>
2014-06-17 19:45:58 +0800

    Update Battle.js

--- Battle.js
+++ Battle.js
@@ -25,6 +25,20 @@ $(document).ready(function(){
 				}
 			}
 		});*/
+		
+		var randomuser = [];
+	var BattleCard = Parse.Object.extend('BattleCard');
+		var query = new Parse.Query(BattleCard);
+		query.include('card','user','flow');
+		query.find({
+			success: function(data) {
+				for(var i=0;i <data.length;i++){
+					randomuser = data[i];
+					console.log(data[i]);
+				}
+			}
+		});
+		
 	}
 	else{
 		alert("請登入");
